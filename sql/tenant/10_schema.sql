b6b1751ac6e68cb02ecb1b3e23f6778dd2e6d3a5
morita <moritasoushi@gmail.com>
2022-07-23 07:27:03 +0000

    create index on sqlite

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -30,3 +30,6 @@ CREATE TABLE player_score (
   created_at BIGINT NOT NULL,
   updated_at BIGINT NOT NULL
 );
+
+create index tenant_compe_row_idx on player_score(tenant_id,competition_id,row_num);
+
